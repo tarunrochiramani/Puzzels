a8ca0654af9cf9d9e9c1eabdc5518795c3fdf3ec
Tarun Rochiramani <tarunrochiramani@gmail.com>
2014-03-16 08:26:18 -0700

    Added robot move + stairs puzzel and their tests.

--- src/main/java/Recursive.java
+++ src/main/java/Recursive.java
@@ -1,3 +1,26 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Point {
+    private int x;
+    private int y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
 
 public class Recursive {
 
@@ -10,5 +33,94 @@ public class Recursive {
             return 1;
         }
         return value * factorial(value -1);
+    }
+
+    public int countWaysDP(int n, int[] map) {
+        if (n < 0) {
+            return 0;
+        }
+
+        if (n == 0) {
+            return 1;
+        }
+
+        if (map[n] > -1) {
+            return map[n];
+        }
+
+        map[n] = countWaysDP(n-1, map) + countWaysDP(n-2, map) + countWaysDP(n-3, map);
+
+        return map[n];
+    }
+
+    public int robotMove(int[][] matrix, int x, int y) {
+        if (matrix == null || matrix.length < x) {
+            return 0;
+        }
+
+        if (x == 0 || y == 0) {
+            return 1;
+        }
+
+        if (matrix[x][y] > 0) {
+            return matrix[x][y];
+        }
+
+        matrix[x][y] = robotMove(matrix, x-1, y) + robotMove(matrix, x, y-1);
+
+        return matrix[x][y];
+    }
+
+    public List<List<Point>> getRobotMove(int size) {
+        if (size <= 0 ) {
+            return null;
+        }
+
+        Map<Point, List<List<Point>>> pointListMap = new HashMap<Point, List<List<Point>>>();
+
+        return getRobotMove(pointListMap, size-1, size-1);
+    }
+
+    private List<List<Point>> getRobotMove(Map<Point, List<List<Point>>> pointListMap, int x, int y) {
+        if (pointListMap == null) {
+            return null;
+        }
+
+        if (x < 0 || y < 0) {
+            return new ArrayList<List<Point>>();
+        }
+
+        Point point = new Point(x, y);
+        if (pointListMap.containsKey(point)) {
+            return pointListMap.get(point);
+        }
+
+        if (x == 0 && y == 0) {
+            List<List<Point>> pointList = new ArrayList<List<Point>>();
+            pointList.add(new ArrayList<Point>());
+            pointList.get(0).add(point);
+
+            pointListMap.put(point, pointList);
+            return pointList;
+        }
+
+        List<List<Point>> downResults = getRobotMove(pointListMap, x - 1, y);
+        List<List<Point>> rightResults = getRobotMove(pointListMap, x, y - 1);
+
+        List<List<Point>> pointMoves = new ArrayList<List<Point>>();
+        addResults(point, downResults, pointMoves);
+        addResults(point, rightResults, pointMoves);
+        pointListMap.put(point, pointMoves);
+
+        return pointMoves;
+    }
+
+    private void addResults(Point point, List<List<Point>> previousStepResults, List<List<Point>> pointMoves) {
+        for (List<Point> result : previousStepResults) {
+            List<Point> pointList = new ArrayList<Point>();
+            pointList.addAll(result);
+            pointList.add(point);
+            pointMoves.add(pointList);
+        }
     }
 }
--- src/test/java/RecursiveTest.java
+++ src/test/java/RecursiveTest.java
@@ -1,7 +1,13 @@
-import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 public class RecursiveTest {
@@ -13,10 +19,90 @@ public class RecursiveTest {
     }
 
 
+    @Test
     public void canFactorialNonNegativeNumber() throws Exception {
         int result = recursive.factorial(4);
 
         assertTrue(result > 0);
         assertEquals(24, result);
     }
+
+    @Test
+    public void canCountWays() {
+        int count = recursive.countWaysDP(4, new int[]{-1, -1 , -1, -1, -1});
+
+        assertTrue(count > 0);
+    }
+
+    @Test
+    public void testRobotMove() {
+        assertEquals(0, recursive.robotMove(null, 0, 0));
+
+        int size = 1;
+        int[][] matrix = new int[size][size];
+        assertEquals(1, recursive.robotMove(matrix, size-1, size-1));
+
+        size = 2;
+        matrix = new int[size][size];
+        int moves = recursive.robotMove(matrix, size - 1, size - 1);
+        assertTrue(moves > 1);
+        assertEquals(2, moves);
+
+        size = 3;
+        matrix = new int[size][size];
+        moves = recursive.robotMove(matrix, size - 1, size - 1);
+        assertTrue(moves > 1);
+        assertEquals(6, moves);
+
+        size = 4;
+        matrix = new int[size][size];
+        moves = recursive.robotMove(matrix, size - 1, size - 1);
+        assertTrue(moves > 1);
+        assertEquals(20, moves);
+    }
+
+    @Test
+    public void canGetRobotMoves() {
+        int size = 0;
+        assertNull(recursive.getRobotMove(size));
+
+        size = 1;
+        List<List<Point>> pointList = recursive.getRobotMove(size);
+        assertNotNull(pointList);
+        assertFalse(pointList.isEmpty());
+        System.out.println("size: " + size + " - " + printMoves(pointList));
+
+        size = 2;
+        pointList = recursive.getRobotMove(size);
+        assertNotNull(pointList);
+        assertFalse(pointList.isEmpty());
+        System.out.println("size: " + size + " - " + printMoves(pointList));
+
+        size = 3;
+        pointList = recursive.getRobotMove(size);
+        assertNotNull(pointList);
+        assertFalse(pointList.isEmpty());
+        System.out.println("size: " + size + " - " + printMoves(pointList));
+    }
+
+    private String printMoves(List<List<Point>> pointList) {
+        if (pointList == null) {
+            System.out.println("Invalid Point list");
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (List<Point> points : pointList) {
+            stringBuilder.append("\n");
+            for (Point point : points) {
+                stringBuilder.append("(");
+                stringBuilder.append(point.getX() + "," + point.getY());
+                stringBuilder.append(") ");
+            }
+
+        }
+
+        return stringBuilder.toString();
+    }
+
 }
